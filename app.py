1a119ca56e361409f8906270f86920369eb49b1e
Fuad Poroshtica <26333597+FuadPoroshtica@users.noreply.github.com>
2019-01-16 09:00:44 +0000

    uploaded Nav

--- app.py
+++ app.py
@@ -5,8 +5,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Halló heimur!'
+    return 'Halló heimur! <br>' \
+           '<a href="/about">about </a>' \
+           '<a href="/biography">Biography </a>' \
+           '<a href="/pictures">Pictures </a>'
 
+@app.route('/biography')
+def Biography():
+    return 'Biography! <br>' \
+           '<a href="/about">about </a> ' \
+           '<a href="/biography">Biography </a>' \
+           '<a href="/pictures">Pictures </a>'
+
+@app.route('/pictures')
+def Pictures():
+    return 'Pictures! <br>' \
+           '<a href="/">about </a> ' \
+           '<a href="/biography">Biography </a> ' \
+           '<a href="/pictures">Pictures </a>'
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
